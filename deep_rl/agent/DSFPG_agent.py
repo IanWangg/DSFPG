345b167b87b8ef3e57264c22c599e96b1c87e266
Yiran <yiran.wang3@mail.mcgill.ca>
2021-06-23 17:05:15 +0800

    DSFPG offline preparation

---
+++ deep_rl/agent/DSFPG_agent.py
@@ -0,0 +1,172 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+from ..network import *
+from ..component import *
+from .BaseAgent import *
+import torchvision
+
+# for offline training
+import d4rl
+
+
+'''
+Difference from DDPG :
+1. Q value is calculated by SF network
+'''
+
+class DSFPGAgent(BaseAgent):
+    def __init__(self, config):
+        BaseAgent.__init__(self, config)
+        self.config = config
+        self.task = config.task_fn()
+        self.network = config.network_fn()
+        self.target_network = config.network_fn()
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.replay = config.replay_fn()
+        self.random_process = config.random_process_fn()
+        self.total_steps = 0
+        self.state = None
+
+        assert config.train_what in ['SF', 'ALL']
+        self.train_what = config.train_what
+
+        # flag that idicate offline/online setting
+        self.offline = config.offline
+        self.dataset_name = config.dataset_name
+        self.dataset = None
+
+        if self.offline:
+            self.env = gym.make(self.dataset_name)
+            self.dataset = self.env.get_dataset()
+            self.offline_replay_init()
+
+
+
+    def offline_replay_init(self):
+        # dataset must be created
+        assert self.dataset is not None
+        actions = self.dataset['actions']
+        observations = self.dataset['observations']
+        terminals = self.dataset['terminals']
+        reward = self.dataset['rewards']
+
+        for i in range(len(reward) - 1):
+            # if the next state is still within the same episode
+            if not terminals[i]:
+                obs = observations[i]
+                action = actions[i]
+                next_obs = observations[i]
+                reward = rewards[i]
+                done = terminals[i+1]
+                self.replay.feed(dict(
+                    state=obs,
+                    action=action,
+                    reward=reward,
+                    next_state=next_obs,
+                    mask=1-np.asarray(done, dtype=np.int32),
+                ))
+
+        print('Offline Replay Buffer Prepared Successfully')
+
+
+
+    def soft_update(self, target, src):
+        for target_param, param in zip(target.parameters(), src.parameters()):
+            target_param.detach_()
+            target_param.copy_(target_param * (1.0 - self.config.target_network_mix) +
+                               param * self.config.target_network_mix)
+
+    def eval_step(self, state):
+        self.config.state_normalizer.set_read_only()
+        state = self.config.state_normalizer(state)
+        action = self.network(state)
+        self.config.state_normalizer.unset_read_only()
+        return to_np(action)
+
+    def step(self):
+        if self.offline:
+            self.step_offline()
+        else:
+            self.step_online()
+
+    def step_offline(self):
+        transitions = self.replay.sample()
+        states = tensor(transitions.state)
+        actions = tensor(transitions.action)
+        rewards = tensor(transitions.reward).unsqueeze(-1)
+        next_states = tensor(transitions.next_state)
+        mask = tensor(transitions.mask).unsqueeze(-1)
+
+
+
+
+
+    def step_online(self):
+        config = self.config
+        if self.state is None:
+            self.random_process.reset_states()
+            self.state = self.task.reset()
+            self.state = config.state_normalizer(self.state)
+
+        if self.total_steps < config.warm_up:
+            action = [self.task.action_space.sample()]
+        else:
+            action = self.network(self.state)
+            action = to_np(action)
+            action += self.random_process.sample()
+        action = np.clip(action, self.task.action_space.low, self.task.action_space.high)
+        next_state, reward, done, info = self.task.step(action)
+        next_state = self.config.state_normalizer(next_state)
+        self.record_online_return(info)
+        reward = self.config.reward_normalizer(reward)
+
+        self.replay.feed(dict(
+            state=self.state,
+            action=action,
+            reward=reward,
+            next_state=next_state,
+            mask=1-np.asarray(done, dtype=np.int32),
+        ))
+
+        if done[0]:
+            self.random_process.reset_states()
+        self.state = next_state
+        self.total_steps += 1
+
+        if self.replay.size() >= config.warm_up:
+            transitions = self.replay.sample()
+            states = tensor(transitions.state)
+            actions = tensor(transitions.action)
+            rewards = tensor(transitions.reward).unsqueeze(-1)
+            next_states = tensor(transitions.next_state)
+            mask = tensor(transitions.mask).unsqueeze(-1)
+
+
+            # need to change the following
+            phi_next = self.target_network.feature(next_states)
+            a_next = self.target_network.actor(phi_next)
+            q_next = self.target_network.critic(phi_next, a_next)
+            q_next = config.discount * mask * q_next
+            q_next.add_(rewards)
+            q_next = q_next.detach()
+            phi = self.network.feature(states)
+            q = self.network.critic(phi, actions)
+            critic_loss = (q - q_next).pow(2).mul(0.5).sum(-1).mean()
+
+            self.network.zero_grad()
+            critic_loss.backward()
+            self.network.critic_opt.step()
+
+            phi = self.network.feature(states)
+            action = self.network.actor(phi)
+            policy_loss = -self.network.critic(phi.detach(), action).mean()
+
+            self.network.zero_grad()
+            policy_loss.backward()
+            self.network.actor_opt.step()
+
+            self.soft_update(self.target_network, self.network)
---
+++ scipts/run_DSFPG.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('../')
+from deep_rl import *
+
+def DSFPG_offline(**kwargs):
+    generate_tag(kwargs)
+    kwargs.setdefault('log_level', 0)
+    config = Config()
+    config.merge(kwargs)
+
+    config.task_fn = lambda: Task(config.game)
+    config.eval_env = config.task_fn()
+    config.max_steps = int(1e6)
+    config.eval_interval = int(1e4)
+    config.eval_episodes = 20
+
+    # for offline RL setting start
+    config.offline = True
+    config.dataset_name = 'hopper-random-v0'
+
+    # offline setting end
+
+    config.network_fn = lambda: DeterministicActorCriticNet(
+        config.state_dim, config.action_dim,
+        actor_body=FCBody(config.state_dim, (400, 300), gate=F.relu),
+        critic_body=FCBody(config.state_dim + config.action_dim, (400, 300), gate=F.relu),
+        actor_opt_fn=lambda params: torch.optim.Adam(params, lr=1e-3),
+        critic_opt_fn=lambda params: torch.optim.Adam(params, lr=1e-3))
+
+    config.replay_fn = lambda: UniformReplay(memory_size=int(1e6), batch_size=100)
+    config.discount = 0.99
+    config.random_process_fn = lambda: OrnsteinUhlenbeckProcess(
+        size=(config.action_dim,), std=LinearSchedule(0.2))
+    config.warm_up = int(1e4)
+    config.target_network_mix = 5e-3
+    run_steps(DSFPGAgent(config))
+
+
+DSFPG_offline(game='Hopper-v2', dataset_name='hopper-random-v0')
